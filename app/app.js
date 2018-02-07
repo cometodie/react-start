19e111abdddcf6d51f6d7342ae7516ce0f6c9af1
olegmusin <superfilch@yandex.by>
2018-02-08 00:55:29 +0300

    add redux extension

--- app/app.js
+++ app/app.js
@@ -23,7 +23,8 @@ const users = (state = initialState, action) => {
     return state; 
 }
 
-const store = createStore(users);
+const store = createStore(users, window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
